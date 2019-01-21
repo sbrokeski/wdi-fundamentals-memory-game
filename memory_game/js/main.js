a17baa890de33412e67b760d84170d353ea6ea3d
John Michael <johnmhairston@hotmail.com>
2019-01-21 14:58:41 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards [0];
+	
+
+cardsInPlay.push(cardOne);
+
+console.log ("User flipped" + cardOne);
+
+
+
+//console.log("User flipped" + cardFour);
+
+
+
+
+
+
+
+
+
+
+
